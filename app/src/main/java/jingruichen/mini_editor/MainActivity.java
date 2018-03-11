494d7c38db864bf2094792b8dcb2ff54dd248aeb
Chen <jingrui.chen@ucdconnect.ie>
2018-03-11 15:00:37 +0800

    18/3/11

--- app/src/main/java/jingruichen/mini_editor/MainActivity.java
+++ app/src/main/java/jingruichen/mini_editor/MainActivity.java
@@ -1,13 +1,25 @@
 package jingruichen.mini_editor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "jingruichen.mini_editor.Message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
